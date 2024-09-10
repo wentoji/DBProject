e61cbeef5f835ed0ba23d7df3a22d623ded34582
wenxi <wx.huang@student.maastrichtuniversity.nl>
2024-09-10 17:42:42 +0200

    Changed some names.

--- src/main/java/com/example/dbproject/Pizza_Restaurant_GUI.java
+++ src/main/java/com/example/dbproject/Pizza_Restaurant_GUI.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Pizza_Restaurant_GUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Pizza_Restaurant_GUI.class.getResource("pizza_restaurant.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
